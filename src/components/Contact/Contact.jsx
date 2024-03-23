8164fb99068e905ab919d1486558571dc5cb6cb6
HalynaMal <dguoit@ukr.net>
2024-03-23 22:01:23 +0200

    end

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,29 @@
-const Contact = ({name, number, id, onDeleteContact}) => {
+import { FaPhoneFlip } from "react-icons/fa6";
+import { FaUserLarge } from "react-icons/fa6";
+import css from "./Contact.module.css";
+
+const Contact = ({ name, number, id, onDeleteContact }) => {
   return (
-    <div>
-    <h3>{name}</h3>
-        <p>{number}</p>
-        <button onClick={() => onDeleteContact(id)}>Delete</button>
+    <div className={css.contactBox}>
+      <div>
+        <p className={css.pText}>
+          <span className={css.icon}>
+            <FaUserLarge />
+          </span>
+          {name}
+        </p>
+        <p className={css.pText}>
+          <span className={css.icon}>
+             <FaPhoneFlip />           
+          </span>
+          {number}
+        </p>
+      </div>
+      <button className={css.btn} onClick={() => onDeleteContact(id)}>
+        Delete
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
-
-
+export default Contact;
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,11 +1,18 @@
 
-const SearchBox = ({value, onSearch}) => {
+import css from "./SearchBox.module.css";
+
+const SearchBox = ({ value, onSearch }) => {
   return (
     <div>
-        <p>Finde contacts by name</p>
-        <input type="text" value={value} onChange={e => onSearch(e.target.value)}/>
+      <p className={css.text}>Find contacts by name</p>
+      <input
+        className={css.inputFilter}
+        type="text"
+        value={value}
+        onChange={(e) => onSearch(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
